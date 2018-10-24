b971449bb30dfd10d8eeffefa4781621b1a1e075
basti_circa <sebastian.stickert@gmail.com>
2018-10-24 18:25:17 +0200

    added and applied GstPipeline as base class

--- gst-py/audio_test_pipeline.py
+++ gst-py/audio_test_pipeline.py
@@ -3,9 +3,11 @@ gi.require_version("Gst", "1.0")
 gi.require_version("Gtk", "3.0")
 gi.require_version("GstVideo", "1.0")
 from gi.repository import Gst, GObject, Gtk
+from gst_pipeline import GstPipeline
 
-class AudioTest(object):
+class AudioTest(GstPipeline):
     def __init__(self):
+        super().__init__("Audio Test")
         self._init_ui()
         self._init_gst_pipe()
 
@@ -17,14 +19,9 @@ class AudioTest(object):
         self.window.show_all()
 
     def _init_gst_pipe(self):
-        self.pipeline = Gst.Pipeline()
+        self.audiosrc = self.make_add_element("audiotestsrc", "audio")
+        self.audiosink = self.make_add_element("alsasink", "sink")
 
-        self.audiosrc = Gst.ElementFactory.make("audiotestsrc", "audio")
-        self.pipeline.add(self.audiosrc)
-
-        self.audiosink = Gst.ElementFactory.make("alsasink", "sink")
-        self.pipeline.add(self.audiosink)
-
-        self.audiosrc.link(self.audiosink)
+        self.link_elements(self.audiosrc, self.audiosink)
 
         self.pipeline.set_state(Gst.State.PLAYING)
--- gst-py/filesrc_viewer.py
+++ gst-py/filesrc_viewer.py
@@ -3,12 +3,12 @@ import gi
 gi.require_version("Gst", "1.0")
 gi.require_version("Gtk", "3.0")
 gi.require_version("GstVideo", "1.0")
-from gi.repository import Gst, GObject, Gtk, GstVideo
+from gi.repository import Gst, Gtk, GstVideo
+from gst_pipeline import GstPipeline
 
-from pprint import pprint
-
-class FilesrcViewer(object):
+class FilesrcViewer(GstPipeline):
     def __init__(self):
+        super().__init__("Filesrc-Viewer")
         self._init_ui()
         self._init_gst_pipe()
 
@@ -31,42 +31,47 @@ class FilesrcViewer(object):
         self.window.show_all()
 
     def _init_gst_pipe(self):
-        self.pipeline = Gst.Pipeline.new("filesrc-viewer-pipeline")
+        # create necessary elements
+        self.filesrc = self.make_add_element("filesrc", "filesrc")
+        self.decoder = self.make_add_element("decodebin", "decoder")
+        self.queue = self.make_add_element("queue", "vid_queue")
+        self.converter = self.make_add_element("videoconvert", "converter")
+        self.videosink = self.make_add_element("autovideosink", "videosink")
 
-        self.filesrc = Gst.ElementFactory.make("filesrc", "filesrc")
-        self.pipeline.add(self.filesrc)
+        # connect element signals
+        self.register_callback(self.decoder, "pad-added", self.decoder_pad_added)
 
-        self.decoder = Gst.ElementFactory.make("decodebin", "decoder")
-        self.decoder.connect("pad-added", self.decoder_pad_added)
-        self.pipeline.add(self.decoder)
-
-        self.queue = Gst.ElementFactory.make("queue", "vid_queue")
-        self.pipeline.add(self.queue)
-
-        self.converter = Gst.ElementFactory.make("videoconvert", "converter")
-        self.pipeline.add(self.converter)
-
-        self.videosink = Gst.ElementFactory.make("autovideosink", "videosink")
-        self.pipeline.add(self.videosink)
-
-        # link filesrc to decoder
-        self.filesrc.link(self.decoder)
+        # setup pipeline links
+        self.link_elements(self.filesrc, self.decoder)
 
         # link queue to converter through to video sink
-        # note: queue will be linked once pads added on decoder
+        # note: queue will be dynamically linked once pad is added on decoder
         # (see self.decoder_pad_added)
-        self.queue.link(self.converter)
-        self.converter.link(self.videosink)
+        self.link_elements(self.queue, self.converter)
+        self.link_elements(self.converter, self.videosink)
 
-        bus = self.pipeline.get_bus()
-        bus.add_signal_watch()
-        bus.enable_sync_message_emission()
-        bus.connect("message", self.on_message)
-        bus.connect("sync-message::element", self.on_sync_message)
+    def on_bus_message(self, bus, message):
+        """ Resets Start button based on playback/error state. """
+        t = message.type
+        if t == Gst.MessageType.EOS:
+            self.pipeline.set_state(Gst.State.NULL)
+            self.button.set_label("Start")
+        elif t == Gst.MessageType.ERROR:
+            self.pipeline.set_state(Gst.State.NULL)
+            err, debug = message.parse_error()
+            print("Error: %s" % err, debug)
+            self.button.set_label("Start")
 
-        #self.pipeline.set_state(Gst.State.PLAYING)
+    def on_bus_sync_message(self, bus, message):
+        """ Sets x window ID once image sink is ready to prepare output. """
+        message_name = message.get_structure().get_name()
+        if message_name == "prepare-window-handle":
+            imagesink = message.src
+            imagesink.set_property("force-aspect-ratio", True)
+            imagesink.set_window_handle(self.movie_window.get_property("window").get_xid())
 
     def start_stop(self, w):
+        """ Toggles playback depending on current play state. """
         if self.button.get_label() == "Start":
             filepath = self.entry.get_text().strip()
             if os.path.isfile(filepath):
@@ -80,38 +85,15 @@ class FilesrcViewer(object):
             self.pipeline.set_state(Gst.State.NULL)
             self.button.set_label("Start")
 
-    def on_message(self, bus, message):
-        t = message.type
-        if t == Gst.MessageType.EOS:
-            self.pipeline.set_state(Gst.State.NULL)
-            self.button.set_label("Start")
-        elif t == Gst.MessageType.ERROR:
-            self.pipeline.set_state(Gst.State.NULL)
-            err, debug = message.parse_error()
-            print("Error: %s" % err, debug)
-            self.button.set_label("Start")
-
     def decoder_pad_added(self, decoder, pad):
-        """ should be triggered once the decoder receives input from the filesrc. """
+        """
+        Link decoder src pad to queue sink pad
+        once the decoder receives input from the filesrc.
+        """
         template_property = pad.get_property("template")
         template_name = template_property.name_template
+        # template name may differ for other decoders/demuxers
         if template_name == "src_%u":
             # link to video queue sink
             queue_sink = self.queue.sinkpads[0]
-            pad.link(queue_sink)
-        ''' # demuxers will probably have other template names
-        if template_name == "video_%02d":
-            # link to video queue sink
-            queue_sink = self.queue.get_pad("sink")
-            pad.link(queue_sink)
-        elif template_name == "audio_%02d":
-            # link to audio queue sink
-            pass
-        '''
-
-    def on_sync_message(self, bus, message):
-        message_name = message.get_structure().get_name()
-        if message_name == "prepare-window-handle":
-            imagesink = message.src
-            imagesink.set_property("force-aspect-ratio", True)
-            imagesink.set_window_handle(self.movie_window.get_property("window").get_xid())
+            self.link_elements(pad, queue_sink)
